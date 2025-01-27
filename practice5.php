846d1d252dc1e6234219e920b49a7deedb9442e7
iam <umekichi3hoshi@gmail.com>
2025-01-28 00:09:31 +0900

    Update practice5.php

--- practice5.php
+++ practice5.php
@@ -1,7 +1,9 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-  $input_name = htmlspecialchars($_POST['input'],ENT_QUOTES,'UTF-8');
-  setcookie("username",$input_name,time()+3600);//クッキーを作る部分
+  if(isset($_POST['input'])){
+    $input_name = htmlspecialchars($_POST['input'],ENT_QUOTES,'UTF-8');
+    setcookie("username",$input_name,time()+3600);//クッキーを作る部分
+  }
 }
 ?>
 <form method="post">
