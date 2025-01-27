f9bc8b9ace851cc4d0a469cb65745a95b835f0aa
iam <umekichi3hoshi@gmail.com>
2025-01-27 21:34:27 +0900

    Update practice4.php

--- practice4.php
+++ practice4.php
@@ -1,4 +1,9 @@
 <?php
+echo "<button type="submit" name="delete_example">"
+if(isset($_POST['delete_example'])){
+	setcookie("username","",time()-3600);
+	echo "クッキーを食べたよ";
+}
 setcookie("username","tarou",time()+3600);
 if(isset($_COOKIE["username"])){
 	echo "hello".$_COOKIE["username"]."さん!!!";
