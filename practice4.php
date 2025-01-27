6f9e5bcc63f9ea7aa8bf12a6279af32d3631093c
iam <umekichi3hoshi@gmail.com>
2025-01-27 23:34:42 +0900

    Update practice4.php

--- practice4.php
+++ practice4.php
@@ -1,14 +1,16 @@
 <?php
-setcookie("username","tarou",time()+3600);
+setcookie("username","tarou",time()+3600);//クッキーを作る部分
 if(isset($_POST['delete_example'])){
-  setcookie("username","",time()-3600);
-  echo "クッキーを食べたよ";
+  setcookie("username","",time()-3600);//一時間クッキーが保たれるらしいよ!秒単位で指定するらしい!
+  echo "クッキーを食べたよ！もうクッキーは無いよ！";
 }
 elseif(isset($_COOKIE["username"])){
-  echo "はろー！".$_COOKIE["username"]."さん!!!";
+  echo "はろー！".$_COOKIE["username"]."さん!!!これがクッキーだよ!";
 }
 else{
-  echo "クッキーが無いぞ!!!🍪もう一度アクセスしてみろ！！！";
+  echo "ぼくちんのクッキーが無いぞ!!!🍪もう一度アクセスしてみろ！！！";
 }
 ?>
-<form method="post"><button type="submit" name="delete_example">削除</button></form>
+<form method="post">
+  <button type="submit" name="delete_example">削除</button>
+</form><!--フォームだよ-->
