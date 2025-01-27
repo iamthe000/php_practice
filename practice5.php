7919e8042397a7482994e36e09bc1df07db4b275
iam <umekichi3hoshi@gmail.com>
2025-01-28 00:03:13 +0900

    Update practice5.php

--- practice5.php
+++ practice5.php
@@ -1,16 +1,20 @@
 <?php
-setcookie("username","tarou",time()+3600);//クッキーを作る部分
-if(isset($_POST['delete_example'])){
-  setcookie("username","",time()-3600);//一時間クッキーが保たれるらしいよ!秒単位で指定するらしい!
-  echo "クッキーを食べたよ！もうクッキーは無いよ！";
-}
-elseif(isset($_COOKIE["username"])){
-  echo "はろー！".$_COOKIE["username"]."さん!!!これがクッキーだよ!";
-}
-else{
-  echo "ぼくちんのクッキーが無いぞ!!!🍪もう一度アクセスしてみろ！！！";
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+  $input_name = htmlspecialchars($_POST['input'],ENT_QUOTES,'UTF-8');
+  setcookie("username",$input_name,time()+3600);//クッキーを作る部分
 }
 ?>
 <form method="post">
-  <button type="submit" name="delete_example">削除</button>
-</form><!--フォームだよ-->
+    <input type="text" id="input" name="input">
+    <button type="submit">go</button>
+</form>
+<?php
+if (isset($_COOKIE['username'])) {
+  $input_name = htmlspecialchars($_POST['input'],ENT_QUOTES,'UTF-8');
+echo "hello".$_COOKIE['username']."!<br>";
+}
+else {
+echo "<center>who are you</center>";
+}
+?>
+</center>
