c4cdd7b55d575fb5aa21128bba673c7e71d8b194
iam <umekichi3hoshi@gmail.com>
2025-01-28 21:59:45 +0900

    Update practice7.php

--- practice7.php
+++ practice7.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
 $_SESSION["変数名"]="代入する値";//これが書式らしいよ！！！すごいね
-
+$_SESSION["user"]="tester";
+$age=$_SESSION["age"]=20;
+$sid=session_id();//セッションidを取得できるよ！！
+echo "セッションidは".$sid."<br>";
+echo "年齢は".$age."です！！";
+/*これセッションにする意味あった？後で分かるのかな...？*/
 ?>
