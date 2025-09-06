6633d0b5004419d284c4f76cae030b18431d3c68
iam <umekichi3hoshi@gmail.com>
2025-09-06 22:30:44 +0900

    Update phpSQL_1(base).php

--- php-sql/phpSQL_1(base).php
+++ php-sql/phpSQL_1(base).php
@@ -47,7 +47,7 @@ echo "データを追加しました";
 
 //vvvvvvvvvvvvvvvvデータの取得vvvvvvvvvvvvvvvv
 
-$sql = "SELECT * FROM users WHERE age > :age";
+$sql = "SELECT * FROM users WHERE age > :age";//$sqlのなかにSQLの指示をそのまま書く
 $stmt = $pdo->prepare($sql);//「これからのstmtは$pdoのprepareメソッドってことだよ〜」
 $stmt->execute([':age' => 18]);//$sqlのなかの「:age」に18を代入して実行する
 
@@ -55,10 +55,4 @@ $results = $stmt->fetchAll(); //stmtによるsqlの実行結果をfetchAll()で�
 foreach ($results as $row) {//foreachで$resultsで取り出した実行結果を1行の情報で$rowで連想配列として保存する
     echo $row['name'] . ' - ' . $row['age'] . "<br>";//「-」で値をつなげて<br>で都度改行している。例えばこれだと「太郎 - 20」として出力される
 }
-/*----------【「$sql」について】----------
-$sqlのなかにSQLの指示をそのまま書く
-*/
-/*----------【「$stmt」について】----------
-$sqlのなかの「:age」に18を代入して実行する
-*/
 ?>
